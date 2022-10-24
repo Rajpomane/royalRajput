d5ce891b3e6595ac35bfc536cf2fa26958098a5e
Rajpomane <95400073+Rajpomane@users.noreply.github.com>
2022-10-24 21:24:46 +0530

    Update __main__.py

--- GJ516X/__main__.py
+++ GJ516X/__main__.py
@@ -7,10 +7,10 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from GJ516X import LOGGER, app, userbot
-from GJ516X.core.call import GJ516
-from GJ516X.plugins import ALL_MODULES
-from GJ516X.utils.database import get_banned_users, get_gbanned
+from 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀 import LOGGER, app, userbot
+from 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.core.call import GJ516
+from 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.plugins import ALL_MODULES
+from 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
 
@@ -23,7 +23,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("GJ516X").error(
+        LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀").error(
             "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
         )
         return
@@ -31,7 +31,7 @@ async def init():
         not config.SPOTIFY_CLIENT_ID
         and not config.SPOTIFY_CLIENT_SECRET
     ):
-        LOGGER("GJ516X").warning(
+        LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀").warning(
             "Spotify Client Id & Secret not added, Chutiya Saala ek itni simple cheej nahi laa paaya."
         )
     try:
@@ -45,28 +45,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("GJ516X.plugins" + all_module)
-    LOGGER("GJ516X.plugins").info(
+        importlib.import_module("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.plugins" + all_module)
+    LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.plugins").info(
         "Necessary Modules Imported Successfully."
     )
     await userbot.start()
-    await GJ516.start()
+    await 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.start()
     try:
-        await GJ516.stream_call(
-            "https://telegra.ph/file/8d5db123638c2f6bb6ce4.mp4"
+        await 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.stream_call(
+            "https://telegra.ph/file/c3a6ac9fcde7bd3703d20.mp4"
         )
     except NoActiveGroupCall:
-        LOGGER("GJ516X").error(
+        LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀").error(
             "[ERROR] - \n\nHey , firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
         )
         sys.exit()
     except:
         pass
-    await GJ516.decorators()
-    LOGGER("GJ516X").info("Music Bot Started Successfully, Now Give your girlfriend chumt to @export_gabbar")
+    await 𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀.decorators()
+    LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀").info("Music Bot Started Successfully, Now Give your girlfriend chumt to @I_LOVE_YOU_PAGAL")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("GJ516X").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
+    LOGGER("𝐈𝛕ᷟ͢𝚣⃪ꙴ ⋆‌⃝⁣🥵🖤Hᴀᴄᴋᴇʀ Hᴇᴀʀᴛ❤️⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟𓆩᪵𓆪 ⏤͟͟͞➖⃟🥀").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
